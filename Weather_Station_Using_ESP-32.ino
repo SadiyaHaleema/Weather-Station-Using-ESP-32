c31a2e52c3e2650a97754d7fb6b81ea41368d3b1
SadiyaHaleema <hsadia.bscs20seecs@seecs.edu.pk>
2022-05-21 11:51:34 +0500

    Temperature,Humidity & Rain Sensoring using 2 sensors

--- Weather_Station_Using_ESP-32.ino
+++ Weather_Station_Using_ESP-32.ino
@@ -1,41 +1,30 @@
-//#include "DHT.h"
-//#define DHT11PIN 16
-//
-//DHT dht(DHT11PIN, DHT11);
-//void setup()
-//{
-//  
-//  Serial.begin(115200);
-///* Start the DHT11 Sensor */
-//  dht.begin();
-//}
-//
-//void loop()
-//{
-//  float humi = dht.readHumidity();
-//  float temp = dht.readTemperature();
-//  Serial.print("Temperature: ");
-//  Serial.print(temp);
-//  Serial.print("ºC ");
-//  Serial.print("Humidity: ");
-//  Serial.println(humi);
-//  delay(1000);
-//}
-
-
-
-
-
-
-// for ESP32 Microcontroller
+#include "DHT.h"
+#define DHT11PIN 16
 #define rainAnalog 35
 #define rainDigital 34
+DHT dht(DHT11PIN, DHT11);
+// for ESP32 Microcontroller
 
-void setup() {
-  Serial.begin(9600);
-  pinMode(rainDigital,INPUT);
+void setup()
+{
+  
+  Serial.begin(115200);
+/* Start the DHT11 Sensor */
+  dht.begin();
+   pinMode(rainDigital,INPUT);
 }
-void loop() {
+
+void loop()
+{
+  float humi = dht.readHumidity();
+  float temp = dht.readTemperature();
+  Serial.print("Temperature: ");
+  Serial.print(temp);
+  Serial.print("ºC ");
+  Serial.print("Humidity: ");
+  Serial.println(humi);
+  delay(1000);
+
   int rainAnalogVal = analogRead(rainAnalog);
   int rainDigitalVal = digitalRead(rainDigital);
 
