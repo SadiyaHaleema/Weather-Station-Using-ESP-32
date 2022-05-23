979dd915568fcf7ef23d9cd6f7f569a6eb362c3a
SadiyaHaleema <hsadia.bscs20seecs@seecs.edu.pk>
2022-05-23 09:33:10 +0500

    Connected to Wifi Network

--- Weather_Station_Using_ESP-32.ino
+++ Weather_Station_Using_ESP-32.ino
@@ -1,3 +1,9 @@
+//#include <WiFiClient.h>
+#include <WiFi.h>
+const char* ssid     = "Hostels Wi-Fi";
+const char* password = "";
+//set web server port 80
+WiFiServer server(80);
 #include "DHT.h"
 #define DHT11PIN 16
 #define rainAnalog 35
@@ -9,6 +15,18 @@ void setup()
 {
   
   Serial.begin(115200);
+ 
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+ 
+  Serial.println("Connected to the WiFi network");
+  Serial.print("IP address:");
+  Serial.println(WiFi.localIP());
+
 /* Start the DHT11 Sensor */
   dht.begin();
    pinMode(rainDigital,INPUT);
@@ -16,6 +34,21 @@ void setup()
 
 void loop()
 {
+
+  if((WiFi.status()==WL_CONNECTED))
+  {
+      Serial.println("You can try to Ping me");
+      delay(5000);
+
+  }
+  else
+  {
+      Serial.println("Connection Lost");
+
+  }
+
+
+  
   float humi = dht.readHumidity();
   float temp = dht.readTemperature();
   Serial.print("Temperature: ");
